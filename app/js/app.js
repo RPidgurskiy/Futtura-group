a004306071cfaa82986ff82f6445a382c38bb638
RPidgurskiy <pidgura17@gmail.com>
2020-12-25 05:37:42 +0200

    first commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-	
+document.addEventListener("DOMContentLoaded", function () {
+
 	// let rectangle = document.querySelector('.hero__rectangles');
 
 	// rectangle.addEventListener('mousemove', function (e) {
@@ -28,37 +28,45 @@ document.addEventListener("DOMContentLoaded", function() {
 		spaceBetween: 30,
 		// If we need pagination
 		pagination: {
-		  el: '.swiper-pagination',
+			el: '.swiper-pagination',
 		},
-	  
+
 		// Navigation arrows
 		navigation: {
-		  nextEl: '.swiper-button-next',
-		  prevEl: '.swiper-button-prev',
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
 		},
-	  
+
 		// And if we need scrollbar
 		scrollbar: {
-		  el: '.swiper-scrollbar',
+			el: '.swiper-scrollbar',
 		},
 		breakpoints: {
 			440: {
-			  slidesPerView: 1,
-			  spaceBetween: 40,
+				slidesPerView: 1,
+				spaceBetween: 40,
 			},
 			640: {
-			  slidesPerView: 2,
-			  spaceBetween: 40,
+				slidesPerView: 2,
+				spaceBetween: 40,
 			},
 			768: {
-			  slidesPerView: 2,
-			  spaceBetween: 40,
+				slidesPerView: 2,
+				spaceBetween: 40,
 			},
 			1024: {
-			  slidesPerView: 2,
-			  spaceBetween: 50,
+				slidesPerView: 2,
+				spaceBetween: 50,
 			},
-		  }
-	  })
-	  AOS.init();
+		}
+	})
+	AOS.init();
+
+	let vector = document.querySelector('.hero__image');
+
+	vector.addEventListener('mousemove', function (e) {
+		let x = e.clientX / window.innerWidth;
+		let y = e.clientY / window.innerHeight;
+		vector.style.transform = 'translate(+' + x * 100 + 'px, +' + y * 100 + 'px)';
+	});
 });
