9e25b655be2e0b8002f61502eb289d82020c101c
RPidgurskiy <pidgura17@gmail.com>
2020-12-25 04:10:48 +0200

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function() {
+	
+	// let rectangle = document.querySelector('.hero__rectangles');
+
+	// rectangle.addEventListener('mousemove', function (e) {
+	//   let x = e.clientX / window.innerWidth;
+	//   let y = e.clientY / window.innerHeight;
+	//   rectangle.style.transform = 'translate(+' + x * 50 + 'px, +' + y * 50 + 'px)';
+	// });
+	$(function () {
+		$('.popup-modal').magnificPopup({
+			type: 'inline',
+			preloader: false,
+			focus: '#username',
+			modal: true
+		});
+		$(document).on('click', '.popup-modal-dismiss', function (e) {
+			e.preventDefault();
+			$.magnificPopup.close();
+		});
+	});
+
+	var mySwiper = new Swiper('.swiper-container', {
+		// Optional parameters
+		direction: 'horizontal',
+		loop: true,
+		slidesPerView: 1,
+		spaceBetween: 30,
+		// If we need pagination
+		pagination: {
+		  el: '.swiper-pagination',
+		},
+	  
+		// Navigation arrows
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		},
+	  
+		// And if we need scrollbar
+		scrollbar: {
+		  el: '.swiper-scrollbar',
+		},
+		breakpoints: {
+			440: {
+			  slidesPerView: 1,
+			  spaceBetween: 40,
+			},
+			640: {
+			  slidesPerView: 2,
+			  spaceBetween: 40,
+			},
+			768: {
+			  slidesPerView: 2,
+			  spaceBetween: 40,
+			},
+			1024: {
+			  slidesPerView: 2,
+			  spaceBetween: 50,
+			},
+		  }
+	  })
+	  AOS.init();
+});
